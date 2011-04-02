6f0fa5c34f8b32c1c2942d0fb231664ecb613bdd
everzet <ever.zet@gmail.com>
2011-04-03 02:42:41 +0300

    updated bootstrap to use autoload.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,11 @@
 <?php
 
-if (is_file(__DIR__.'/../autoload.php')) {
-  require_once __DIR__ . '/../autoload.php';
-} else {
-  require_once __DIR__ . '/../autoload.php.dist';
-}
+/*
+ * This file is part of the Behat\Mink.
+ * (c) Konstantin Kudryashov <ever.zet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__ . '/../autoload.php';
