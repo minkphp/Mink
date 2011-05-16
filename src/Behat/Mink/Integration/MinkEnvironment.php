b32e31d72ab09feeffcc587c60826c3388d33e30
everzet <ever.zet@gmail.com>
2011-05-17 00:26:07 +0300

    updated bundled steps to reflect api changes of mink and session

--- src/Behat/Mink/Integration/MinkEnvironment.php
+++ src/Behat/Mink/Integration/MinkEnvironment.php
@@ -5,6 +5,7 @@ namespace Behat\Mink\Integration;
 use Behat\Behat\Environment\Environment;
 
 use Behat\Mink\Mink,
+    Behat\Mink\Session,
     Behat\Mink\Driver\GoutteDriver,
     Behat\Mink\Driver\SahiDriver;
 
@@ -35,7 +36,7 @@ class MinkEnvironment extends Environment
     {
         $world = $this;
 
-        $world->drivers = array();
+        $world->sessions  = array();
         $world->getPathTo = function($path) use($world) {
             $startUrl = rtrim($world->getParameter('start_url'), '/') . '/';
 
@@ -54,7 +55,7 @@ class MinkEnvironment extends Environment
 
         if (null === $mink) {
             $mink = new Mink();
-            $this->registerMinkDrivers($mink);
+            $this->registerMinkSessions($mink);
         }
 
         return $mink;
@@ -63,33 +64,34 @@ class MinkEnvironment extends Environment
     /**
      * Returns current Mink session.
      *
+     * @param   string|null name of the session OR active session will be used
+     *
      * @return  Behat\Mink\Session
      */
-    public function getSession()
+    public function getSession($name = null)
     {
-        return $this->getMink()->getSession();
+        return $this->getMink()->getSession($name);
     }
 
     /**
-     * Registers drivers on mink.
+     * Registers sessions on mink.
      *
      * @param   Behat\Mink\Mink $mink
      */
-    private function registerMinkDrivers(Mink $mink)
+    private function registerMinkSessions(Mink $mink)
     {
         if (null === $this->getParameter('start_url')) {
             throw new \InvalidArgumentException('Specify start_url environment parameter');
         }
-        $startUrl       = $this->getParameter('start_url');
-        $defaultDriver  = $this->getParameter('default_driver') ?: 'goutte';
-        $browser        = $this->getParameter('browser') ?: 'firefox';
+        $startUrl = $this->getParameter('start_url');
+        $browser  = $this->getParameter('browser') ?: 'firefox';
 
         $config = $this->getParameter('goutte', array());
         $goutte = new GoutteClient(
             isset($config['zend_config'])       ? $config['zend_config']        : array(),
             isset($config['server_parameters']) ? $config['server_parameters']  : array()
         );
-        $mink->registerDriver('goutte', new GoutteDriver($startUrl, $goutte), 'goutte' === $defaultDriver);
+        $mink->registerSession('goutte', new Session(new GoutteDriver($startUrl, $goutte)));
 
         $config = $this->getParameter('sahi', array());
         $client = new SahiClient(new SahiConnection(
@@ -97,11 +99,11 @@ class MinkEnvironment extends Environment
             isset($config['host'])  ? $config['host']   : 'localhost',
             isset($config['port'])  ? $config['port']   : 9999
         ));
-        $mink->registerDriver('sahi', new SahiDriver($startUrl, $browser, $client), 'sahi' === $defaultDriver);
+        $mink->registerSession('sahi', new Session(new SahiDriver($startUrl, $browser, $client)));
 
 
-        foreach ($this->drivers as $alias => $driver) {
-            $mink->registerDriver($alias, $driver, $driver === $this->getDefaultDriverName());
+        foreach ($this->sessions as $name => $session) {
+            $mink->registerSession($name, $session);
         }
     }
 }
