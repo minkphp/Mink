9b3f58888ef5f526776508e4b069708c75b32450
everzet <ever.zet@gmail.com>
2011-11-04 12:40:09 +0200

    soft-reset in `SeleniumDriver::reset()` instead of reloading

--- src/Behat/Mink/Driver/SeleniumDriver.php
+++ src/Behat/Mink/Driver/SeleniumDriver.php
@@ -122,8 +122,11 @@ class SeleniumDriver implements DriverInterface
      */
     public function reset()
     {
-        $this->stop();
-        $this->start();
+        try {
+            $this->executeScript(
+                '(function(){var c=document.cookie.split(";");for(var i=0;i<c.length;i++){var e=c[i].indexOf("=");var n=e>-1?c[i].substr(0,e):c[i];document.cookie=n+"=;expires=Thu, 01 Jan 1970 00:00:00 GMT";}})()'
+            );
+        } catch(\Exception $e) {}
     }
 
     /**
