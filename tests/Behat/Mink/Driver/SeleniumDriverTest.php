3757c25cb5290031c1e2d0f9637017f61010d3b5
Benjamin Steininger <robo47@robo47.net>
2011-11-25 21:25:15 +0100

    Groups for drivers Testssuites

--- tests/Behat/Mink/Driver/SeleniumDriverTest.php
+++ tests/Behat/Mink/Driver/SeleniumDriverTest.php
@@ -4,6 +4,9 @@ namespace Tests\Behat\Mink\Driver;
 
 require_once 'JavascriptDriverTest.php';
 
+/**
+ * @group seleniumdriver
+ */
 class SeleniumDriverTest extends JavascriptDriverTest
 {
     protected function setUp()
