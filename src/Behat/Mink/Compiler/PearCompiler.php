b84d137f3eab6e940d94b94338a25b5bf3293a2b
Anthon Pang <anthonp@nationalfibre.net>
2012-05-15 12:31:42 -0400

    switch to instaclick fork of php-webdriver

--- src/Behat/Mink/Compiler/PearCompiler.php
+++ src/Behat/Mink/Compiler/PearCompiler.php
@@ -71,7 +71,7 @@ class PearCompiler
             ->in($this->libPath . '/vendor/alexandresalome')
             ->in($this->libPath . '/vendor/behat')
             ->in($this->libPath . '/vendor/fabpot')
-            ->in($this->libPath . '/vendor/facebook')
+            ->in($this->libPath . '/vendor/instaclick')
             ->in($this->libPath . '/vendor/kriswallsmith')
             ->in($this->libPath . '/vendor/zendframework/zend-uri')
             ->in($this->libPath . '/vendor/zendframework/zend-http')
