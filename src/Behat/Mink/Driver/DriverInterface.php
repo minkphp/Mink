0929c73b498963c713ea4efa898850d5a92a5783
Handrus Stephan Nogueira <handrus@gmail.com>
2013-06-12 12:25:18 -0300

    Adding support for maximize browser's window

--- src/Behat/Mink/Driver/DriverInterface.php
+++ src/Behat/Mink/Driver/DriverInterface.php
@@ -374,4 +374,11 @@ interface DriverInterface
      * @param string $name window name (null for the main window)
      */
     public function resizeWindow($width, $height, $name = null);
+
+    /**
+     * Maximize the window if it is not maximized already
+     *
+     * @param string $name window name (null for the main window)
+     */
+    public function maximizeWindow($name = null);
 }
