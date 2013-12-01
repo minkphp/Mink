7edd0b89272d168bd7b2cb77efd58b158d65ecd7
Alexander Obuhovich <aik.bold@gmail.com>
2013-12-01 14:47:39 +0200

    Set cookie to `/` path to please headless drivers

--- tests/Behat/Mink/Driver/web-fixtures/cookie_page1.php
+++ tests/Behat/Mink/Driver/web-fixtures/cookie_page1.php
@@ -1,5 +1,5 @@
 <?php
-    setcookie('srvr_cookie', 'srv_var_is_set');
+    setcookie('srvr_cookie', 'srv_var_is_set', null, '/');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru">
