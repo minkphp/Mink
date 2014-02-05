076af3853b5425414f619e4cb1ec225f83f84514
Christophe Coevoet <stof@notk.org>
2014-02-05 09:59:59 +0100

    Added an assertion on the image using GD

--- tests/Behat/Mink/Driver/GeneralDriverTest.php
+++ tests/Behat/Mink/Driver/GeneralDriverTest.php
@@ -992,12 +992,24 @@ OUT;
 
     public function testScreenshot()
     {
+        if (!extension_loaded('gd')) {
+            $this->markTestSkipped('Testing screenshots requires the GD extension');
+        }
+
         $this->getSession()->visit($this->pathTo('/index.php'));
 
         $screenShot = $this->getSession()->getScreenshot();
 
         $this->assertInternalType('string', $screenShot);
         $this->assertFalse(base64_decode($screenShot, true), 'The returned screenshot should not be base64-encoded');
+
+        $img = imagecreatefromstring($screenShot);
+
+        if (false === $img) {
+            $this->fail('The screenshot should be a valid image');
+        }
+
+        imagedestroy($img);
     }
 
     protected function pathTo($path)
