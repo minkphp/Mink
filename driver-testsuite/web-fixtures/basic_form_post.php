a9a9cd73adef4b0111b7d599b4429c7a5e804457
Christophe Coevoet <stof@notk.org>
2014-05-16 23:00:44 +0200

    Changed the DOCTYPE to html instead of XHTML 1.1 in all fixtures
    Closes #533

--- driver-testsuite/web-fixtures/basic_form_post.php
+++ driver-testsuite/web-fixtures/basic_form_post.php
@@ -1,6 +1,5 @@
-<?php  ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru">
+<!DOCTYPE html>
+<html>
 <head>
     <title>Basic Form Saving</title>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
