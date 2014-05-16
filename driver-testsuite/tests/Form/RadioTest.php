958bc4730e3100900d397ffcb0ad5ac55cfb5c2b
Christophe Coevoet <stof@notk.org>
2014-05-17 01:41:39 +0200

    Moved a test to the RadioTest class as it works with radio buttons

--- driver-testsuite/tests/Form/RadioTest.php
+++ driver-testsuite/tests/Form/RadioTest.php
@@ -73,4 +73,20 @@ class RadioTest extends TestCase
         $this->assertEquals('test', $option1->getValue());
         $this->assertEquals('test3', $option2->getValue());
     }
+
+    /**
+     * @see https://github.com/Behat/MinkSahiDriver/issues/32
+     */
+    public function testSetValueXPathEscaping()
+    {
+        $session = $this->getSession();
+        $session->visit($this->pathTo('/advanced_form.html'));
+        $page = $session->getPage();
+
+        $sex = $page->find('xpath', '//*[@name = "sex"]' . "\n|\n" . '//*[@id = "sex"]');
+        $this->assertNotNull($sex, 'xpath with line ending works');
+
+        $sex->setValue('m');
+        $this->assertEquals('m', $sex->getValue(), 'no double xpath escaping during radio button value change');
+    }
 }
