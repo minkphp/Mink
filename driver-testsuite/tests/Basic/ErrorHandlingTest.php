91f10a467af3b16788ac5dad3536673809e594ae
Alexander Obuhovich <aik.bold@gmail.com>
2014-07-13 00:22:43 +0300

    Except any exception when non-existing element is being searched

--- driver-testsuite/tests/Basic/ErrorHandlingTest.php
+++ driver-testsuite/tests/Basic/ErrorHandlingTest.php
@@ -10,7 +10,7 @@ class ErrorHandlingTest extends TestCase
 
     const NOT_FOUND_EXCEPTION = 'Exception';
 
-    const INVALID_EXCEPTION = 'Behat\Mink\Exception\DriverException';
+    const INVALID_EXCEPTION = 'Exception';
 
     public function testVisitErrorPage()
     {
