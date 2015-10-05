42f94230ffce2bb1c1f3a795eb8d5dfb60ce8b60
Daniel Wehner <daniel@tag1consulting.com>
2015-10-05 12:47:53 +0200

    fix cs

--- driver-testsuite/tests/Basic/CookieTest.php
+++ driver-testsuite/tests/Basic/CookieTest.php
@@ -47,7 +47,8 @@ class CookieTest extends TestCase
         $this->assertContains('Previous cookie: NO', $this->getSession()->getPage()->getText());
     }
 
-    public function testCookieWithSemicolon() {
+    public function testCookieWithSemicolon()
+    {
         $this->getSession()->setCookie('srvr_cookie', 'foo;bar;baz');
         $this->getSession()->visit($this->pathTo('/cookie_page2.php'));
         $this->assertEquals('foo;bar;baz', $this->getSession()->getCookie('srvr_cookie'));
