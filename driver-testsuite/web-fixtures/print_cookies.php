48f6d699bfa357722ba702723bfe344d92d7022d
Greg Knaddison <greg@knaddison.com>
2015-10-12 08:43:04 -0600

    Fix a few more instances of xss

--- driver-testsuite/web-fixtures/print_cookies.php
+++ driver-testsuite/web-fixtures/print_cookies.php
@@ -5,6 +5,12 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
 </head>
 <body>
+	<?php
+	foreach ($_COOKIE as $key => $value) {
+		unset($_COOKIE[$key]);
+		$_COOKIE[htmlspecialchars($key, ENT_QUOTES, 'UTF-8')] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+	}
+	?>
     <?php echo str_replace('>', '', var_export($_COOKIE, true)); ?>
 </body>
 </html>
