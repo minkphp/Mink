0766c3281ba45582dfef550b306c340ab6d14bd4
Dottyx <me.dotty@gmail.com>
2017-11-18 12:18:20 +0200

    Create bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+include_once __DIR__.'/../vendor/autoload.php';
+
+$classLoader = new \Composer\Autoload\ClassLoader();
+$classLoader->addPsr4("Your\\Test\\Namespace\\Here\\", __DIR__, true);
+$classLoader->register();
+
+// PHPUnit 6 introduced a breaking change that
+// removed PHPUnit_Framework_TestCase as a base class,
+// and replaced it with \PHPUnit\Framework\TestCase
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase'))
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
