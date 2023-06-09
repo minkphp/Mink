9996c393965659915e48e1ba226923ace0fbb777
Christophe Coevoet <stof@notk.org>
2023-06-09 14:00:25 +0200

    Add constants for the supported key modifiers

---
+++ src/KeyModifier.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Behat\Mink;
+
+final class KeyModifier
+{
+    public const CTRL = 'ctrl';
+    public const ALT = 'alt';
+    public const SHIFT = 'shift';
+    public const META = 'meta';
+}
