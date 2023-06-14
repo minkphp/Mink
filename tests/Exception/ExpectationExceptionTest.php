f85081a90983e8d75a13e9f73c42f60272344b71
Christophe Coevoet <stof@notk.org>
2023-06-14 11:03:31 +0200

    Add a test covering ExpectationException for drivers without status code

--- tests/Exception/ExpectationExceptionTest.php
+++ tests/Exception/ExpectationExceptionTest.php
@@ -2,7 +2,9 @@
 
 namespace Behat\Mink\Tests\Exception;
 
+use Behat\Mink\Driver\DriverInterface;
 use Behat\Mink\Exception\ExpectationException;
+use Behat\Mink\Exception\UnsupportedDriverActionException;
 use PHPUnit\Framework\TestCase;
 
 class ExpectationExceptionTest extends TestCase
@@ -34,6 +36,38 @@ class ExpectationExceptionTest extends TestCase
 Expectation failure
 
 +--[ HTTP/1.1 200 | http://localhost/test | %s ]
+|
+|  <body>
+|  <h1>Hello world</h1>
+|  <p>Test</p>
+|  </body>
+|
+TXT;
+
+        $expected = sprintf($expected.'  ', get_class($driver));
+
+        $exception = new ExpectationException('Expectation failure', $driver);
+
+        $this->assertEquals($expected, $exception->__toString());
+    }
+
+    public function testUnsupportedStatusCode()
+    {
+        $driver = $this->createStub(DriverInterface::class);
+
+        $driver->method('getStatusCode')
+            ->willThrowException(new UnsupportedDriverActionException('Status code is not supported.', $driver));
+        $driver->method('getCurrentUrl')
+            ->willReturn('http://localhost/test');
+
+        $html = "<html><head><title>Hello</title></head>\n<body>\n<h1>Hello world</h1>\n<p>Test</p>\n</body></html>";
+        $driver->method('getContent')
+            ->willReturn($html);
+
+        $expected = <<<'TXT'
+Expectation failure
+
++--[ http://localhost/test | %s ]
 |
 |  <body>
 |  <h1>Hello world</h1>
