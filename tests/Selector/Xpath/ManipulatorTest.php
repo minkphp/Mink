73e3de4cc279ff65463f9b2047eacd19bfae3bb9
Christophe Coevoet <stof@notk.org>
2023-06-14 10:54:12 +0200

    Add tests covering the handling of invalid XPath when prepending

--- tests/Selector/Xpath/ManipulatorTest.php
+++ tests/Selector/Xpath/ManipulatorTest.php
@@ -65,6 +65,22 @@ class ManipulatorTest extends TestCase
                 "some_tag[(contains(normalize-space(string(.)), 'foo|bar') | other_tag[contains(./@some_attribute, 'foo|bar')])]",
                 "some_xpath/some_tag[(contains(normalize-space(string(.)), 'foo|bar') | other_tag[contains(./@some_attribute, 'foo|bar')])]",
             ),
+            // Invalid XPath queries should be handled gracefully to let the DOMQuery report a proper failure for them later
+            'unclosed string literal single quote' => array(
+                'some_xpath',
+                "some_tag1 | some_tag2[@foo = 'bar]",
+                "some_xpath/some_tag1 | some_tag2[@foo = 'bar]",
+            ),
+            'unclosed string literal double quote' => array(
+                'some_xpath',
+                'some_tag1 | some_tag2[@foo = "bar]',
+                'some_xpath/some_tag1 | some_tag2[@foo = "bar]',
+            ),
+            'unclosed bracket' => array(
+                'some_xpath',
+                'some_tag1 | some_tag2[@foo = "bar"',
+                'some_xpath/some_tag1 | some_tag2[@foo = "bar"',
+            ),
         );
     }
 }
